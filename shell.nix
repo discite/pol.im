4251ff20a5334f115c41931b038c6120cf77f8c0
imurillus <4948877+discite@users.noreply.github.com>
2023-06-02 21:50:07 -0500

    updating nix to be deterministic

--- shell.nix
+++ shell.nix
@@ -1,7 +1,8 @@
 let
   # Rolling updates, not deterministic.
-  pkgs = import (fetchTarball ("channel:nixpkgs-unstable")) { };
+  pkgs = import (fetchTarball ("channel:nixpkgs-unstable")) { config.allowUnfree = true; };
 in
 pkgs.mkShell {
-  buildInputs = [ pkgs.cargo pkgs.rustc ];
+  buildInputs = with pkgs; [ pkgconfig openssl gcc cargo rustc rust-analyzer vscode vscode-extensions.rust-lang.rust-analyzer ];
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
