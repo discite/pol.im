1c5bf6108a98919a701c526372b14c2ac1299fcb
imurillus <4948877+discite@users.noreply.github.com>
2023-06-03 14:15:50 -0500

    adding needed commands to program the API

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pol.im
 Axum simple site, i want to avoid JS as much as i can
+
+- In Terminal 1: `cargo watch -q -c -w src/ -x run`
+- In Terminal 2: `cargo watch -q -c -w tests/ -x 'test -q quick_dev -- --nocapture'`
--- shell.nix
+++ shell.nix
@@ -3,6 +3,6 @@ let
   pkgs = import (fetchTarball ("channel:nixpkgs-unstable")) { config.allowUnfree = true; };
 in
 pkgs.mkShell {
-  buildInputs = with pkgs; [ pkgconfig openssl gcc cargo rustc rust-analyzer vscode vscode-extensions.rust-lang.rust-analyzer ];
+  buildInputs = with pkgs; [ pkgconfig openssl gcc cargo cargo-watch rustc rust-analyzer vscode vscode-extensions.rust-lang.rust-analyzer ];
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
